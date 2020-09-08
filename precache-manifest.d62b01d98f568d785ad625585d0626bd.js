8e036c8483b9bb9bb16ef70e8e6cc04977682ef1
David Tian <davidtian@Davids-MacBook-Pro-462.local>
2020-09-08 05:48:47 -0500

    Updates

---
+++ precache-manifest.d62b01d98f568d785ad625585d0626bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7384fd943f4e9322d2020ca9d76f65e5",
+    "url": "/first_react_tutorial/index.html"
+  },
+  {
+    "revision": "3918ad5f3f750c7f79e6",
+    "url": "/first_react_tutorial/static/css/main.0fb20b90.chunk.css"
+  },
+  {
+    "revision": "4eb78c3393e557c45185",
+    "url": "/first_react_tutorial/static/js/2.75cd4b5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first_react_tutorial/static/js/2.75cd4b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3918ad5f3f750c7f79e6",
+    "url": "/first_react_tutorial/static/js/main.e006f748.chunk.js"
+  },
+  {
+    "revision": "dd6106a3a10b6e3ca9a4",
+    "url": "/first_react_tutorial/static/js/runtime-main.ec47b624.js"
+  }
+]);
